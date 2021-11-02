80332c1f6cb7003b6961a07422e87dc727f6557d
Llane00 <bluetinestyle@126.com>
2021-11-02 16:10:58 +0800

    add README.md

--- test/index.test.js
+++ test/index.test.js
@@ -5,8 +5,6 @@ import { insertionSort } from '../index'
 let unSortArray = randomArray();
 // 给随机数组排序
 let sortArray = unSortArray.sort((a, b) => a - b);
-// let unSortArray = [3, 2, 1]
-// let sortArray = [1,2,3]
 
 describe('fizzbuzz', () => {
   test('insertSort 测试插入排序', () => {
