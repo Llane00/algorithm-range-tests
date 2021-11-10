71fe1bc9395b8c567a4b117486e5701f8c5b9ec6
Llane00 <bluetinestyle@126.com>
2021-11-10 19:15:17 +0800

    feat: fix test bug

--- test/index.test.js
+++ test/index.test.js
@@ -16,43 +16,45 @@ import {
 // 生成随机数组
 let unSortArray = randomArray(1000);
 // 给随机数组排序
-let sortArray = unSortArray.sort((a, b) => a - b);
+let copyArray = JSON.parse(JSON.stringify(unSortArray))
+let sortArray = copyArray.sort((a, b) => a - b);
+
 
 describe('排序算法测试', () => {
   test('bubbleSort 冒泡排序', () => {
-    expect(bubbleSort(unSortArray)).toStrictEqual(sortArray)
+    expect(bubbleSort(unSortArray)).toEqual(sortArray)
   })
 
   test('selectionSort 选择排序', () => {
-    expect(selectionSort(unSortArray)).toStrictEqual(sortArray)
+    expect(selectionSort(unSortArray)).toEqual(sortArray)
   })
 
   test('insertSort 插入排序', () => {
-    expect(insertionSort(unSortArray)).toStrictEqual(sortArray)
+    expect(insertionSort(unSortArray)).toEqual(sortArray)
   })
 
   test('mergeSort 归并排序', () => {
-    expect(mergeSort(unSortArray)).toStrictEqual(sortArray)
+    expect(mergeSort(unSortArray)).toEqual(sortArray)
   })
 
   test('easyQuickSort 快速排序简化版', () => {
-    expect(easyQuickSort(unSortArray)).toStrictEqual(sortArray)
+    expect(easyQuickSort(unSortArray)).toEqual(sortArray)
   })
 
   test('quickSort 快速排序完整版', () => {
-    expect(quickSort(unSortArray)).toStrictEqual(sortArray)
+    expect(quickSort(unSortArray)).toEqual(sortArray)
   })
 
   test('countingSort 计数排序', () => {
-    expect(countingSort(unSortArray)).toStrictEqual(sortArray)
+    expect(countingSort(unSortArray)).toEqual(sortArray)
   })
 
   test('bucketSort 桶排序', () => {
-    expect(bucketSort(unSortArray)).toStrictEqual(sortArray)
+    expect(bucketSort(unSortArray)).toEqual(sortArray)
   })
 
   test('radioSort 基数排序', () => {
-    expect(radioSort(unSortArray)).toStrictEqual(sortArray)
+    expect(radioSort(unSortArray)).toEqual(sortArray)
   })
 
 })
